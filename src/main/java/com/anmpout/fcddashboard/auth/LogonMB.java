1bc2f3c97095dae9ebe63bf3e9ea8e2edcfc451b
aboutovinas <antonis@schoox.com>
2019-03-20 19:42:22 +0200

    fist configuration

---
+++ src/main/java/com/anmpout/fcddashboard/auth/LogonMB.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.anmpout.fcddashboard.auth;
+
+import static com.anmpout.fcddashboard.utils.Utils.addDetailMessage;
+import com.github.adminfaces.template.config.AdminConfig;
+import com.github.adminfaces.template.session.AdminSession;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Specializes;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.omnifaces.util.Faces;
+
+/**
+ *
+ * @author anmpout
+ */
+@Named
+@SessionScoped
+@Specializes
+public class LogonMB extends AdminSession implements Serializable {
+
+    private String currentUser;
+    private String email;
+    private String password;
+    private boolean remember;
+    @Inject
+    private AdminConfig adminConfig;
+
+
+    public void login() throws IOException {
+        currentUser = email;
+        addDetailMessage("Logged in successfully as <b>" + email + "</b>");
+        Faces.getExternalContext().getFlash().setKeepMessages(true);
+        Faces.redirect(adminConfig.getIndexPage());
+    }
+
+    @Override
+    public boolean isLoggedIn() {
+
+        return currentUser != null;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isRemember() {
+        return remember;
+    }
+
+    public void setRemember(boolean remember) {
+        this.remember = remember;
+    }
+
+    public String getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(String currentUser) {
+        this.currentUser = currentUser;
+    }
+}
---
+++ src/main/java/com/anmpout/fcddashboard/utils/Utils.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.anmpout.fcddashboard.utils;
+
+import java.io.Serializable;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.application.FacesMessage;
+import org.omnifaces.util.Messages;
+
+/**
+ *
+ * @author anmpout
+ */
+@ApplicationScoped
+public class Utils implements Serializable {
+       public static void addDetailMessage(String message) {
+        addDetailMessage(message, null);
+    }
+
+    public static void addDetailMessage(String message, FacesMessage.Severity severity) {
+
+        FacesMessage facesMessage = Messages.create("").detail(message).get();
+        if (severity != null && severity != FacesMessage.SEVERITY_INFO) {
+            facesMessage.setSeverity(severity);
+        }
+        Messages.add(null, facesMessage);
+    }
+}
