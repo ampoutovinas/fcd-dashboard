f8d547f39e0d0a183278ce8104403a541674d62b
aboutovinas <antonis@schoox.com>
2019-03-28 23:16:43 +0200

    add charts

--- src/main/java/com/anmpout/fcddashboard/controller/PathDetailsController.java
+++ src/main/java/com/anmpout/fcddashboard/controller/PathDetailsController.java
@@ -21,11 +21,15 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
-import org.primefaces.model.chart.*;
-import org.primefaces.model.charts.*;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.charts.line.LineChartModel;
+import org.primefaces.model.charts.ChartData;
 import org.primefaces.model.charts.axes.cartesian.CartesianScales;
 import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearAxes;
 import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearTicks;
+import org.primefaces.model.charts.bar.BarChartModel;
+import org.primefaces.model.charts.line.LineChartDataSet;
+import org.primefaces.model.charts.line.LineChartOptions;
 import org.primefaces.model.charts.optionconfig.legend.Legend;
 import org.primefaces.model.charts.optionconfig.legend.LegendLabel;
 import org.primefaces.model.charts.optionconfig.title.Title;
@@ -47,15 +51,20 @@ public class PathDetailsController implements Serializable {
     private String monthYearSelection;
     private Calendar statisticsDate;
     private Date month;
-    private org.primefaces.model.chart.BarChartModel speed;
-    private org.primefaces.model.charts.bar.BarChartModel count;
+    private Date day;
+    private BarChartModel count;
+    private LineChartModel speed;
+    private LineChartModel time;
     private static final SimpleDateFormat MONTH_YEAR_FORMAT = new SimpleDateFormat("MM/yyyy");
     private boolean showDailyStatistics = false;
-       private boolean showMonthlyStatistics = false;
+    private boolean showMonthlyStatistics = false;
+     private boolean enableSelectDayButton = false; 
+       
           @PostConstruct
     public void init() {
         showDailyStatistics = false;
-        showMonthlyStatistics =false;
+        showMonthlyStatistics = false;
+        enableSelectDayButton = false;
         pathId=1;
         pathDistance="";
         region="";
@@ -68,9 +77,20 @@ public class PathDetailsController implements Serializable {
        path = service1.getPath(pathId);
        setupFields();
        pathJSONString = Utils.createPathJSONString(path.getPoints());
-       createBarModel();
          createBarModel2();
+        // createLineModel();
+         speed = new LineChartModel();
+         time = new LineChartModel();
     }
+
+    public boolean isEnableSelectDayButton() {
+        return enableSelectDayButton;
+    }
+
+    public void setEnableSelectDayButton(boolean enableSelectDayButton) {
+        this.enableSelectDayButton = enableSelectDayButton;
+    }
+
 
 
 
@@ -130,13 +150,46 @@ public class PathDetailsController implements Serializable {
         this.statisticsDate = statisticsDate;
     }
 
-    public org.primefaces.model.charts.bar.BarChartModel getCount() {
+    public BarChartModel getCount() {
         return count;
     }
 
-    public void setCount(org.primefaces.model.charts.bar.BarChartModel count) {
+    public void setCount(BarChartModel count) {
         this.count = count;
     }
+    
+        public Date getMonth() {
+        return month;
+    }
+
+    public void setMonth(Date month) {
+        this.month = month;
+    }
+
+    public Date getDay() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+    public LineChartModel getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(LineChartModel speed) {
+        this.speed = speed;
+    }
+
+    public LineChartModel getTime() {
+        return time;
+    }
+
+    public void setTime(LineChartModel time) {
+        this.time = time;
+    }
+    
     
     
 
@@ -149,8 +202,7 @@ public class PathDetailsController implements Serializable {
         pathDistance = Double.toString(Utils.convertMetersToKm(path.getPathDistance()))+" Km";
     }
 
-
-    
+   
     public Integer getInitialStatisticsYear() {
         List<Integer> yearsWithStatistics = new ArrayList<>();     
         if(yearsWithStatistics == null || yearsWithStatistics.isEmpty()) {
@@ -162,22 +214,6 @@ public class PathDetailsController implements Serializable {
      public void updateStatistics() throws ParseException {
 
 }   
-
-    public Date getMonth() {
-        return month;
-    }
-
-    public void setMonth(Date month) {
-        this.month = month;
-    }
-
-    public org.primefaces.model.chart.BarChartModel getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(org.primefaces.model.chart.BarChartModel speed) {
-        this.speed = speed;
-    }
 
     public boolean isShowDailyStatistics() {
         return showDailyStatistics;
@@ -195,53 +231,121 @@ public class PathDetailsController implements Serializable {
         this.showMonthlyStatistics = showMonthlyStatistics;
     }
     
-    
-    
-      private void createBarModel() {
-        speed = initBarModel();
- 
-        speed.setTitle("Bar Chart");
-        speed.setLegendPosition("ne");
- 
-        Axis xAxis = speed.getAxis(AxisType.X);
-        xAxis.setLabel("Gender");
- 
-        Axis yAxis = speed.getAxis(AxisType.Y);
-        yAxis.setLabel("Births");
-        yAxis.setMin(0);
-        yAxis.setMax(200);
+    public void createBarModel2() {
+    count = new org.primefaces.model.charts.bar.BarChartModel();
+    ChartData data = new ChartData();
+
+    org.primefaces.model.charts.bar.BarChartDataSet barDataSet = new org.primefaces.model.charts.bar.BarChartDataSet();
+    barDataSet.setLabel("My First Dataset");
+
+    List<Number> values = new ArrayList<>();
+    values.add(65);
+    values.add(59);
+    values.add(80);
+    values.add(81);
+    values.add(56);
+    values.add(55);
+    values.add(40);
+    values.add(65);
+    values.add(59);
+    values.add(80);
+    values.add(81);
+    values.add(56);
+    values.add(55);
+    values.add(40);
+    values.add(65);
+    values.add(59);
+    values.add(80);
+    values.add(81);
+    values.add(56);
+    values.add(55);
+    values.add(40);
+    values.add(56);
+    values.add(55);
+    values.add(40);
+    barDataSet.setData(values);
+
+    List<String> bgColor = new ArrayList<>();
+    bgColor.add("rgb(255, 99, 132, 0.8)");
+    // barDataSet.setBackgroundColor(bgColor);
+
+    List<String> borderColor = new ArrayList<>();
+    borderColor.add("rgb(255, 99, 132)");
+    // barDataSet.setBorderColor(borderColor);
+    barDataSet.setBorderWidth(1);
+
+    data.addChartDataSet(barDataSet);
+
+    List<String> labels = new ArrayList<>();
+    labels.add("00");
+    labels.add("01");
+    labels.add("02");
+    labels.add("03");
+    labels.add("04");
+    labels.add("05");
+    labels.add("06");
+    labels.add("07");
+    labels.add("08");
+    labels.add("09");
+    labels.add("10");
+    labels.add("11");
+    labels.add("12");
+    labels.add("13");
+    labels.add("14");
+    labels.add("15");
+    labels.add("16");
+    labels.add("17");
+    labels.add("18");
+    labels.add("19");
+    labels.add("20");
+    labels.add("21");
+    labels.add("22");
+    labels.add("23");
+    data.setLabels(labels);
+    count.setData(data);
+
+    //Options
+    org.primefaces.model.charts.bar.BarChartOptions options = new org.primefaces.model.charts.bar.BarChartOptions();
+    CartesianScales cScales = new CartesianScales();
+    CartesianLinearAxes linearAxes = new CartesianLinearAxes();
+    CartesianLinearTicks ticks = new CartesianLinearTicks();
+    ticks.setBeginAtZero(true);
+    linearAxes.setTicks(ticks);
+    cScales.addYAxesData(linearAxes);
+    options.setScales(cScales);
+
+    Title title = new Title();
+    title.setDisplay(true);
+    title.setText("Bar Chart");
+    options.setTitle(title);
+
+    Legend legend = new Legend();
+    legend.setDisplay(true);
+    legend.setPosition("top");
+    LegendLabel legendLabels = new LegendLabel();
+    legendLabels.setFontStyle("bold");
+    legendLabels.setFontColor("#2980B9");
+    legendLabels.setFontSize(24);
+    legend.setLabels(legendLabels);
+    options.setLegend(legend);
+
+    count.setOptions(options);
     }
-        private org.primefaces.model.chart.BarChartModel initBarModel() {
-        org.primefaces.model.chart.BarChartModel model = new org.primefaces.model.chart.BarChartModel();
- 
-        ChartSeries boys = new ChartSeries();
-        boys.setLabel("Boys");
-        boys.set("2004", 120);
-        boys.set("2005", 100);
-        boys.set("2006", 44);
-        boys.set("2007", 150);
-        boys.set("2008", 25);
- 
-        ChartSeries girls = new ChartSeries();
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 110);
-        girls.set("2007", 135);
-        girls.set("2008", 120);
- 
-        model.addSeries(boys);
-        model.addSeries(girls);
- 
-        return model;
+
+    public void buttonAction() {
+      showDailyStatistics = true;
     }
-        public void createBarModel2() {
-        count = new org.primefaces.model.charts.bar.BarChartModel();
+    public void onDaySelect(SelectEvent event) {
+        enableSelectDayButton = false;
+
+    }
+            
+        public void createLineModel() {
+        speed = new LineChartModel();
+         time = new LineChartModel();
         ChartData data = new ChartData();
          
-        org.primefaces.model.charts.bar.BarChartDataSet barDataSet = new org.primefaces.model.charts.bar.BarChartDataSet();
-        barDataSet.setLabel("My First Dataset");
-         
+        LineChartDataSet dataSet = new LineChartDataSet();
         List<Number> values = new ArrayList<>();
         values.add(65);
         values.add(59);
@@ -250,93 +354,34 @@ public class PathDetailsController implements Serializable {
         values.add(56);
         values.add(55);
         values.add(40);
-        values.add(65);
-        values.add(59);
-        values.add(80);
-        values.add(81);
-        values.add(56);
-        values.add(55);
-        values.add(40);
-        values.add(65);
-        values.add(59);
-        values.add(80);
-        values.add(81);
-        values.add(56);
-        values.add(55);
-        values.add(40);
-        values.add(56);
-        values.add(55);
-        values.add(40);
-        barDataSet.setData(values);
-         
-        List<String> bgColor = new ArrayList<>();
-        bgColor.add("rgb(255, 99, 132, 0.8)");
-       // barDataSet.setBackgroundColor(bgColor);
-         
-        List<String> borderColor = new ArrayList<>();
-        borderColor.add("rgb(255, 99, 132)");
-       // barDataSet.setBorderColor(borderColor);
-        barDataSet.setBorderWidth(1);
-         
-        data.addChartDataSet(barDataSet);
+        dataSet.setData(values);
+        dataSet.setFill(false);
+        dataSet.setLabel("My First Dataset");
+        dataSet.setBorderColor("rgb(75, 192, 192)");
+        dataSet.setLineTension(0.1);
+        data.addChartDataSet(dataSet);
          
         List<String> labels = new ArrayList<>();
-        labels.add("00");
-        labels.add("01");
-        labels.add("02");
-        labels.add("03");
-        labels.add("04");
-        labels.add("05");
-        labels.add("06");
-        labels.add("07");
-        labels.add("08");
-        labels.add("09");
-        labels.add("10");
-        labels.add("11");
-        labels.add("12");
-        labels.add("13");
-        labels.add("14");
-        labels.add("15");
-        labels.add("16");
-        labels.add("17");
-        labels.add("18");
-        labels.add("19");
-        labels.add("20");
-        labels.add("21");
-        labels.add("22");
-        labels.add("23");
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        labels.add("July");
         data.setLabels(labels);
-        count.setData(data);
          
         //Options
-        org.primefaces.model.charts.bar.BarChartOptions options = new org.primefaces.model.charts.bar.BarChartOptions();
-     CartesianScales cScales = new CartesianScales();
-       CartesianLinearAxes linearAxes = new CartesianLinearAxes();
-        CartesianLinearTicks ticks = new CartesianLinearTicks();
-        ticks.setBeginAtZero(true);
-        linearAxes.setTicks(ticks);
-        cScales.addYAxesData(linearAxes);
-        options.setScales(cScales);
-         
+        LineChartOptions options = new LineChartOptions();        
         Title title = new Title();
         title.setDisplay(true);
-        title.setText("Bar Chart");
+        title.setText("Line Chart");
         options.setTitle(title);
- 
-        Legend legend = new Legend();
-        legend.setDisplay(true);
-        legend.setPosition("top");
-        LegendLabel legendLabels = new LegendLabel();
-        legendLabels.setFontStyle("bold");
-        legendLabels.setFontColor("#2980B9");
-        legendLabels.setFontSize(24);
-        legend.setLabels(legendLabels);
-        options.setLegend(legend);
- 
-        count.setOptions(options);
-    }
-        
-            public void buttonAction() {
-              showDailyStatistics = true;
+         
+        speed.setOptions(options);
+        speed.setData(data);
+        time.setOptions(options);
+        time.setData(data);
+       
     }
 }
